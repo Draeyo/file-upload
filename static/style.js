28bf006da4ba242bf7a8c875f2dc18b09d2b4f89
Draeyo <draeyo@gmail.com>
2017-11-30 14:46:53 +0100

    update

--- static/style.js
+++ static/style.js
@@ -1,7 +1,3 @@
-$("input[type='file']").click(function () {
-		$("input[id='file']").click();
-});
-
 $("input[id='file']").change(function (e) {
 		var $this = $(this);
 		$this.next().html($this.val().split('\\').pop());
